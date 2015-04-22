761487ef3d13cf60de233dc7c992f45fb1fafbe7
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-04-22 21:34:11 +0200

    Project init

---
+++ src/DependencyInjection/SLLHIsoCodesValidatorExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SLLH\IsoCodesValidatorBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * Class SLLHIsoCodesValidatorExtension
+ *
+ * @author Sullivan Senechal <soullivaneuh@gmail.com>
+ */
+class SLLHIsoCodesValidatorExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
---
+++ src/SLLHIsoCodesValidatorBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SLLH\IsoCodesValidatorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class SLLHIsoCodesValidatorBundle
+ *
+ * @author Sullivan Senechal <soullivaneuh@gmail.com>
+ */
+class SLLHIsoCodesValidatorBundle extends Bundle
+{
+}
