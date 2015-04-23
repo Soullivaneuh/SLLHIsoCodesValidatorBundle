be34dd0eaba77239ed2383b71fb15c382d640d9c
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-04-23 23:17:27 +0200

    Remove unused use statement

--- src/DependencyInjection/SLLHIsoCodesValidatorExtension.php
+++ src/DependencyInjection/SLLHIsoCodesValidatorExtension.php
@@ -3,7 +3,6 @@
 namespace SLLH\IsoCodesValidatorBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
