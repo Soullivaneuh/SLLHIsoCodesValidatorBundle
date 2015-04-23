7ae55fb69bfb27ad6429e3c83e447e4aa644e1e4
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-04-23 16:47:26 +0200

    Fix php 5.3 syntax error

--- tests/SLLHIsoCodesValidatorBundleTest.php
+++ tests/SLLHIsoCodesValidatorBundleTest.php
@@ -23,10 +23,11 @@ class SLLHIsoCodesValidatorBundleTest extends AbstractContainerBuilderTestCase
     {
         $this->bundle->build($this->container);
 
-        $this->assertCount(2, $this->container->getCompilerPassConfig()->getPasses(), 'Bundle must contains a TranslationPass instance.');
+        $passes = $this->container->getCompilerPassConfig()->getPasses();
+        $this->assertCount(2, $passes, 'Bundle must contains a TranslationPass instance.');
         $this->assertInstanceOf(
             'SLLH\IsoCodesValidatorBundle\DependencyInjection\Compiler\TranslationPass',
-            $this->container->getCompilerPassConfig()->getPasses()[1],
+            $passes[1],
             'Bundle must contains a TranslationPass instance.'
         );
     }
